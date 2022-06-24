bdd3094c7f98cdef2624bec49b032409fc946204
isammt <bella.trigo14@gmail.com>
2022-06-24 20:57:07 -0300

    2 files modified

--- src/app/isabella-home-component/isabella-home-component.component.ts
+++ src/app/isabella-home-component/isabella-home-component.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { IsabellaListServiceService } from '../isabella-list-service.service';
 
 @Component({
   selector: 'app-isabella-home-component',
   templateUrl: './isabella-home-component.component.html',
-  styleUrls: ['./isabella-home-component.component.css']
+  styleUrls: ['./isabella-home-component.component.css'],
 })
 export class IsabellaHomeComponentComponent implements OnInit {
+  constructor(public servico: IsabellaListServiceService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
